04190cfc5f06cc9b17880a9f276a403be0c30cef
Arthur Himmel <himmel.arthur@gmail.com>
2014-11-20 15:47:13 +0100

    Added route for projects

--- me/app.js
+++ me/app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/photos', photos);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
